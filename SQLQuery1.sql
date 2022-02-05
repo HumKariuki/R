151c07a2e1acc720597a1b3f742d11ad61ebe017
Humphrey Kariuki <hkariuki531@gmail.com>
2022-02-05 10:27:18 +0300

    Basic Sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,35 @@
+/* DMl Data manupulation language
+DDL -- Data definition language 
+DCL-- Data control language
+TCL --Transaction control language
+DQL -- Data query language
+
+DML -- changes the data and the database 
+the commands include the following 
+insert, update, delete
+insert-- insert is used for adding the records into the datavase tables
+updates-- it is used for modifying the existing codes
+delete -- it will delete the records from tables NB if use dont use where clause ie where you want 
+the data to be deleted it will delete all recods from the table
+
+
+DDL -- Used for the folliwing 
+Create, Alter , Drop
+create-- we can create specific objects in database like tables, functions, databases, procedures etc
+Alter -- used for modifying the metadata information of specific objects in database
+Drop-- it is used for deleting entire objects with structure from database
+
+
+DCL --
+Grant and Revoke are commands used for DCL (For permissions)
+
+TCL
+The commands used are commit and Rollback
+By default SQL server auto commit the sql statements
+commit -- it will commit the data permanetly in database after we cont rollback the data
+Rollback-- It will re-load the previous data
+
+
+DQL 
+command used is select
+select -- Used to view the table data
