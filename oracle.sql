dc31d674c03793d48a1ab2ae6209d4975928c265
Humphrey Kariuki <hkariuki531@gmail.com>
2022-02-23 19:34:39 +0300

    sql

--- oracle.sql
+++ oracle.sql
@@ -13,6 +13,7 @@ insert into customers (id,name,age,address,salary)VALUES (3,'Jecinta',20,'Murara
 insert into customers (id,name,age,address,salary)VALUES (4,'Mary',22,'Murarandia',70000.00);
 insert into customers (id,name,age,address,salary)VALUES (5,'Rose',55,'Murarandia2',20000.00);
 insert into customers (id,name,age,address,salary)VALUES (6,'Lucy',17,'Kangema',13000.00);
+insert into customers (id,name,age,address,salary)VALUES (7,'Ken',67,'Thika',303000.00);
 
 SELECT
     * FROM
